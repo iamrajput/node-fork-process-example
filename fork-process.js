78c19382176285acc210edee818eefafb180b084
Atul Kumar Singh <atul.kumar@codelogicx.com>
2022-11-07 21:49:52 +0530

    new updates

--- fork-process.js
+++ fork-process.js
@@ -2,18 +2,24 @@ const express = require('express')
 const {fork} = require('child_process')
 const app = express()
 
-
+// normal funtion call (takes around 10.088745682 s)
 app.get('/first',(req,res) => {
    const sum = longTimeTaking()
-   res.send({sum:sum})
+   res.send({sum:sum})  // 
 })
-//coz it's start on  the same process the time and all are same
+//coz it's start on  the same process the time and all are same async call
+//(takes around 10.088745682 s)
+// this is coz it's running on the same process that's the reason 
+// the time taken is same
 app.get('/second',async(req,res) => {
 const sum = await longTimeTakingWithPromise()
    res.send({sum:sum})
 })
 
-
+//here we are using node.js child process fork
+// 0.018008409 s 
+// as it is running as a different process it will not stop the main thread
+// and process the task in less time
 app.get('/three',(req,res) => {
     const child =  fork('./longTask.js') //run on different thread
     child.send('start')
